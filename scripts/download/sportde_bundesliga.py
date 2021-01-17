1de9784b494457936d2692b8aa2c693e392042b3
omlnaut <oneironaut.oml@gmail.com>
2021-01-17 07:35:06 +0100

    trying to scrape premier league

--- scripts/download/sportde_bundesliga.py
+++ scripts/download/sportde_bundesliga.py
@@ -3,8 +3,9 @@ from betting.scraping import *
 from pathlib import Path
 from multiprocessing import Pool
 
-save_path = Path('../../data/sportde/bundesliga')
-start_link = 'https://www.sport.de/fussball/deutschland-bundesliga/se580/2006-2007/ergebnisse-und-tabelle'
+save_path = Path('../../data/sportde/premier_league/raw')
+#start_link = 'https://www.sport.de/fussball/deutschland-bundesliga/se580/2006-2007/ergebnisse-und-tabelle'
+start_link = 'https://www.sport.de/fussball/england-premier-league/se18350/2015-2016/ergebnisse-und-tabelle/'
 start_bs = get_html(start_link)
 season_urls = season_links_from_page(start_bs)
 
@@ -14,8 +15,9 @@ def process_season(info):
 	season, url = info
 	print(season)
 	standings, matchdays, games = scrape_season(season, url)
-	save_season(standings, matchdays, games, 'bundesliga', save_path)
+	save_season(standings, matchdays, games, 'premier_league', save_path)
 
 if __name__=='__main__':
-	with Pool(6) as p:
-		p.map(process_season, target_seasons)
+	process_season(target_seasons[0])
+	#with Pool(6) as p:
+	#	p.map(process_season, target_seasons)

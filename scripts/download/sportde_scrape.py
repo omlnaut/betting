88ee35f2323d6642904ea4dd0faa458d2f0a8d37
omlnaut <oneironaut.oml@gmail.com>
2021-01-25 20:47:53 +0100

    team matching via goals, serie_a downloaded

--- scripts/download/sportde_scrape.py
+++ scripts/download/sportde_scrape.py
@@ -3,11 +3,12 @@ from betting.scraping import *
 from pathlib import Path
 from multiprocessing import Pool
 
-save_path = Path('../../data/sportde/league_one/raw')
+save_path = Path('../../data/sportde/serie_a/raw')
 #start_link = 'https://www.sport.de/fussball/deutschland-bundesliga/se580/2006-2007/ergebnisse-und-tabelle'
 #start_link = 'https://www.sport.de/fussball/england-premier-league/se18350/2015-2016/ergebnisse-und-tabelle/'
 #start_link = 'https://www.sport.de/fussball/spanien-primera-division/se18343/2015-2016/ergebnisse-und-tabelle/'
-start_link = 'https://www.sport.de/fussball/frankreich-ligue-1/se7127/2011-2012/ergebnisse-und-tabelle/'
+#start_link = 'https://www.sport.de/fussball/frankreich-ligue-1/se7127/2011-2012/ergebnisse-und-tabelle/'
+start_link = 'https://www.sport.de/fussball/italien-serie-a/se9033/2012-2013/ergebnisse-und-tabelle/'
 start_bs = get_html(start_link)
 season_urls = season_links_from_page(start_bs)
 
@@ -17,7 +18,7 @@ def process_season(info):
 	season, url = info
 	print(season)
 	standings, matchdays, games = scrape_season(season, url, expected_matchdays=None)
-	save_season(standings, matchdays, games, 'league_one', save_path)
+	save_season(standings, matchdays, games, 'serie_a', save_path)
 
 if __name__=='__main__':
 	with Pool(6) as p:
